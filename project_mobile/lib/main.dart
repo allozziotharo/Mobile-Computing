139132e52bcf3bf6859343d35f6e6f9fbe1fd90b
allozziotharo <01fgermano@gmail.com>
2023-11-03 15:30:03 +0100

    schermata home
    inserimento layout a stack delle sezioni
    colorate di rosso (temporaneo era per capire come si inserisce il colore)
    opzioni sezione se tieni premuto
    finestra di dialogo "sei sicuro?" per l'eliminazione

--- project_mobile/lib/main.dart
+++ project_mobile/lib/main.dart
@@ -1,125 +1,242 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PROGETTO',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'FABRIZIO ROMANO HERE WE GO!'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> sectionTitles = ['Sezione 1', 'Sezione 2'];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Benvenuto'),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView(
+        children: <Widget>[
+          for (int i = 0; i < sectionTitles.length; i++)
+            SectionWidget(
+              title: sectionTitles[i],
+              onTap: () {
+                // Implementa la logica per toccare una sezione esistente, se necessario
+              },
+              onLongPress: () {
+                _showSectionContextMenu(sectionTitles[i]);
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          AddSectionButton(
+            onTap: () {
+              _showAddSectionDialog();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showAddSectionDialog() {
+    String newSectionTitle = '';
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Inserisci il Titolo della Nuova Sezione'),
+          content: TextField(
+            onChanged: (value) {
+              newSectionTitle = value;
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('ANNULLA'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (newSectionTitle.isNotEmpty) {
+                  setState(() {
+                    sectionTitles.add(newSectionTitle);
+                  });
+                }
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
             ),
           ],
+        );
+      },
+    );
+  }
+
+  void _showSectionContextMenu(String title) {
+    String newSectionTitle = title;
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Opzioni della Sezione'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.edit),
+                title: Text('Rinomina Sezione'),
+                onTap: () {
+                  Navigator.of(context).pop(); // Chiudi la finestra di dialogo corrente
+                  _showRenameSectionDialog(title);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.delete),
+                title: Text('Elimina Sezione'),
+                onTap: () {
+                  Navigator.of(context).pop(); // Chiudi la finestra di dialogo corrente
+                  _showDeleteSectionDialog(title);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _showRenameSectionDialog(String sectionTitle) {
+    String newSectionTitle = sectionTitle;
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Rinomina Sezione'),
+          content: TextField(
+            onChanged: (value) {
+              newSectionTitle = value;
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('ANNULLA'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (newSectionTitle.isNotEmpty) {
+                  setState(() {
+                    sectionTitles.remove(sectionTitle);
+                    sectionTitles.add(newSectionTitle);
+                  });
+                }
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDeleteSectionDialog(String sectionTitle) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Sei sicuro?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('NO'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  sectionTitles.remove(sectionTitle);
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text('SI'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class SectionWidget extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+  final VoidCallback onLongPress;
+
+  SectionWidget({required this.title, required this.onTap, required this.onLongPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      onLongPress: onLongPress,
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.red,
+        padding: EdgeInsets.all(16.0),
+        margin: EdgeInsets.all(8.0),
+        child: Text(
+          title,
+          style: TextStyle(fontSize: 20.0, color: Colors.white),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class AddSectionButton extends StatelessWidget {
+  final VoidCallback onTap;
+
+  AddSectionButton({required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 40.0,
+        height: 40.0,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Text(
+          '+',
+          style: TextStyle(fontSize: 24.0, color: Colors.white),
+        ),
+      ),
     );
   }
 }
