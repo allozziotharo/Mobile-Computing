841d54d69d56b830eca2a4e3950a214c032ce315
allozziotharo <01fgermano@gmail.com>
2024-03-08 17:49:30 +0100

    listview dei tool in una sezione
    ho creato la listview
    per provarla ora è spostata sulla home
    la list view è popolata staticamente da 3 voceTool
    voceTool è il widget colonna composto da: titolo che l'utente vuole dare all'istanza del Tool e contenuto del Tool effettivo

--- project_mobile/lib/main.dart
+++ project_mobile/lib/main.dart
@@ -12,7 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< Updated upstream
       home: HomeScreen(), //poi andrà impostato lo splashscreen come schermata iniziale, ora si comincia con la home
+=======
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          textTheme:
+              GoogleFonts.lobsterTwoTextTheme(Theme.of(context).textTheme)),
+      onGenerateRoute: route.controller, //crea il controller nel file route
+      initialRoute:
+          route.signupPage, //la prima pagina visualizzata è quella di login
+>>>>>>> Stashed changes
     );
   }
 }
---
+++ project_mobile/lib/screens/homepage.dart
@@ -0,0 +1,138 @@
+//materiale standard
+import 'package:flutter/material.dart';
+//import per usare route
+import 'package:project_mobile/route/route.dart' as route;
+
+//import che mi serve per provare ToolContainer.dart
+import 'package:project_mobile/widget/ToolContainer.dart';
+
+//creo il widget NavBar menù laterale
+class NavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        //rimuovere il padding rende la sidebar più carina
+        padding: EdgeInsets.zero,
+        //tutti gli elementi che compongono la sidebar
+        children: [
+          //prima parte del menu quella che contieni gli info dell'user
+          UserAccountsDrawerHeader(
+            accountName: const Text('Germano'),
+            accountEmail: const Text('fragermano@numberone.com'),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.asset(
+                  //immagine salvata sul dispositivo si usa in questo modo
+                  'assets/prova.jpg', //devi prima aggiungere la risorsa nella cartella assets
+                  width: 90,
+                  height: 90,
+                  fit: BoxFit.cover,
+                ), //la parte di clipOval credo si possa togliere e lasciare solo CircleAvatar
+              ),
+            ), //Circle avatar
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(//così si prende una risorsa online
+                    'https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg'),
+              ),
+            ),
+          ), //userheader
+
+          //lista delle sezioni del menù laterale
+          //IN EVIDENZA
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text('In Evidenza'),
+            onTap: () => null,
+          ),
+          //LINK
+          ListTile(
+            leading: Icon(Icons.share),
+            title: Text('Links'),
+            onTap: () => null,
+          ),
+          //CALENDARIO
+          ListTile(
+            leading: Icon(Icons.date_range),
+            title: Text('Calendario'),
+            onTap: () => Navigator.pushNamed(context, route.calendarPage),
+          ),
+          //NOTIFICHE
+          ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('Notifiche'),
+            onTap: () => null,
+            trailing: ClipOval(
+              child: Container(
+                color: Colors.red,
+                width: 20,
+                height: 20,
+                child: Center(
+                  child: Text(
+                    '1', //ora metto uno per comodità qui bisogna aggiungere la logica per cui si aggiunge una notifica
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          //divider aggiunge una linea di separazione
+          Divider(),
+          //IMPOSTAZIONI
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Impostazioni'),
+            onTap: () => Navigator.pushNamed(context, route.settingsPage),
+          ),
+          Divider(),
+          //LOGOUT
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => Navigator.pushNamed(context, route.loginPage),
+          ),
+          //fine lista
+        ],
+      ),
+    ); //qui finisce drawer e si fa il return -> ;
+  } //qui finisce widget build
+} //qui finisce la classe NavBar
+
+//screen che contiene un menu laterale a scomparsa
+class HomePage extends StatefulWidget {
+  //costruttore privato
+  HomePage._();
+  //istanza unica
+  static HomePage _instance = HomePage._();
+  //metodo factory
+  factory HomePage() {
+    return _instance;
+  }
+
+  int counter = 0;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavBar(), //widget creato sopra -> menu laterale
+      appBar: AppBar(
+        title: const Text(
+            'HomePage'), //se metto text come child di center viene un po' sulla dx a causa dell'icona del menu
+      ),
+      body: Center(
+        child: listaTool,
+      )
+    );
+  }
+}
