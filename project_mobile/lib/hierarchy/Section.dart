bf0ba1d2bcb2babb23ef2479585804b9b7954ba6
allozziotharo <01fgermano@gmail.com>
2024-03-15 12:43:28 +0100

    settaggio firebase
    settaggio firebase

--- project_mobile/lib/hierarchy/Section.dart
+++ project_mobile/lib/hierarchy/Section.dart
@@ -1,77 +1,50 @@
-import 'package:project_mobile/hierarchy/toolpool/PoolFunzionalita.dart';
-import 'package:flutter/material.dart';
+import 'package:project_mobile/hierarchy/toolpool/Tool.dart';
 
-class Section implements PoolFunzionalita {
-  String titolo;
-  DateTime?
-      data; //IL PUNTO INTERROGATIVO SIGNIFICA CHE IL CAMPO PUO ANCHE ESSERE NULL
-  List<Section>? sezioniFiglie;
-  List<PoolFunzionalita>?
-      toolImplementati; //in Dart non ci sono wildcard: in Java avrei dovuto scrive <? extends PoolFunzionalita>
+class Section {
 
-  Section({required this.titolo, this.data});
+  late String sectionTitle;
+  late DateTime dataCreazione;
+  late List<Tool> toolList;
+ 
+  //modelliamo le sezioni come un albero arbitrario,
+  //annidamento di figli/aggiunta di fratelli
+  late List<Section> childSections;
+  Section? parentSection; //nullable per evitare errori nella sezione radice (quelle di profondità 0)
+  
 
-  String get getTitolo => this.titolo;
+  //COSTRUTTORE (PADRE+TITOLO)
+  Section(Section? parentSection, String newSectionTitle) {
+    this.parentSection = parentSection;
+    this.sectionTitle = newSectionTitle;
+    //creazione di una lista CHE PUO' CAMBIARE DIMENSIONE
+    this.toolList = new List.empty(growable: true);
+    this.childSections = new List.empty(growable: true);
 
-  List<Section>? getSezioniFiglie() {
-    return this.sezioniFiglie;
+    //ORA SEMPRE INIZIALIZZATA
+    this.dataCreazione = DateTime.now();
   }
 
-  List<PoolFunzionalita>? getToolImplementati() {
-    return this.toolImplementati;
+
+  //getters & setters
+  String getSectionTitle() => sectionTitle;
+  void setSectionTitle(String newSectionTitle) {this.sectionTitle = newSectionTitle;}
+
+  DateTime getData() => dataCreazione;
+
+  List<Tool> getToolList() => toolList;
+  List<Section> getChildSections() => childSections;
+
+  Section? getParentSection() => parentSection;
+
+
+  void addSection(String newSectionTitle) {
+    Section newSection = new Section(this.parentSection, newSectionTitle);
+    this.childSections.add(newSection);
+
   }
 
-  @override
-  Future<Section?> aggiungiSezione(BuildContext context, String titolo) async {
-    /*
-La sintassi Future<Section?> indica che il metodo aggiungiSezione restituirà un oggetto Future di tipo Section?.
-Vediamo cosa significa ciascuna parte:
+  
 
-Future: Un Future rappresenta un valore o un errore che potrebbe non essere disponibile immediatamente.
-    È utilizzato per gestire operazioni asincrone, come la lettura di dati da un file o una chiamata di rete,
-    in cui il risultato potrebbe richiedere del tempo per essere ottenuto.
 
-<Section?>: Questa è la tipizzazione generica del Future.
-    Indica che il valore restituito dal Future sarà di tipo Section?.
-    Il punto interrogativo (?) dopo Section indica che il tipo può essere nullable, cioè il valore può essere null.
 
-Quindi, Future<Section?> indica un futuro risultato di tipo Section?, indicando che il risultato potrebbe essere un oggetto Section o null.
-    */
-    showDialog(
-      context: context,
-      builder: (context) {
-        String nuovoTitolo = '';
-
-        return AlertDialog(
-          title: Text('Inserisci il titolo della nuova sezione'),
-          content: TextField(
-            onChanged: (value) {
-              nuovoTitolo = value;
-            },
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('ANNULLA'),
-            ),
-            TextButton(
-              onPressed: () {
-                if (nuovoTitolo.isNotEmpty) {
-                  Section nuovaSezione = Section(titolo: nuovoTitolo);
-                  sezioniFiglie?.add(
-                      nuovaSezione); //metti'?' perché è un campo potenzialmente null
-                  Navigator.of(context).pop(nuovaSezione);
-                }
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  //poi bisogna fare un override di ogni funzione presente nell'interfaccia, PER IMPLEMENTARNE IL CORPO QUI
 }
--- project_mobile/lib/main.dart
+++ project_mobile/lib/main.dart
@@ -2,10 +2,19 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+
 //import del controller per la navigazione
 import 'package:project_mobile/route/route.dart' as route;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
