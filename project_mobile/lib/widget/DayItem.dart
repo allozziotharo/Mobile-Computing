28ac6167d613469f23b39ec35cce448c6a323241
andrea <andrealippo01@gmail.com>
2024-05-04 17:10:18 +0200

    ultime modifiche

--- project_mobile/lib/widget/DayItem.dart
+++ project_mobile/lib/widget/DayItem.dart
@@ -85,38 +85,166 @@ class ClassItemWidget extends StatelessWidget {
   }
 
   //funzione che costruisce il widget da visualizzare vero
-  Widget BuildItem(BuildContext context) => Container(
-        margin: EdgeInsets.all(8),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: Colors
-                .amber //eventualmente implementare un modo per far scegliere all'utente il colore
-            ),
-        child: ListTile(
-          contentPadding: EdgeInsets.all(16),
-          leading: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("${item.subject}",
-                  style: TextStyle(
-                      fontSize:
-                          16)), //se qui aumento la size errore di overflow
-              Text("${item.start}", style: TextStyle(fontSize: 14)),
-              Text("${item.end}", style: TextStyle(fontSize: 14)),
-            ],
-          ),
-          title: Center(
-            child: Text("${item.notes}", style: TextStyle(fontSize: 16)),
-          ),
-          trailing: IconButton(
-            icon: Icon(
-              Icons.delete,
-              color: Colors.red,
-              size: 32,
-            ),
-            onPressed:
-                onClicked, //richiamerà removeItem nella classe weekPageState
-          ),
+  Widget BuildItem(BuildContext context) => Padding(
+        padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          primary: false,
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          children: [
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 12),
+              child: Container(
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue[50],
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 3,
+                      color: Color(0x33000000),
+                      offset: Offset(
+                        0,
+                        1,
+                      ),
+                    )
+                  ],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(4, 0, 0, 0),
+                            child: Text(item.subject,
+                                style: TextStyle(
+                                  fontFamily: 'Outfit',
+                                  color: Color(0xFF14181B),
+                                  fontSize: 30,
+                                  letterSpacing: 0,
+                                  fontWeight: FontWeight.w500,
+                                )),
+                          ),
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 0, 8, 0),
+                                  child: Card(
+                                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                                    color: Color(0x4DEE8B60),
+                                    elevation: 0,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    child: Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          8, 4, 8, 4),
+                                      child: Text(item.start,
+                                          style: TextStyle(
+                                            fontFamily: 'Plus Jakarta Sans',
+                                            color: Color(0xFFEE8B60),
+                                            fontSize: 14,
+                                            letterSpacing: 0,
+                                            fontWeight: FontWeight.normal,
+                                          )),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 0, 8, 0),
+                                  child: Card(
+                                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                                    color: Color(0x4DEE8B60),
+                                    elevation: 0,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    child: Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          8, 4, 8, 4),
+                                      child: Text(item.end,
+                                          style: TextStyle(
+                                            fontFamily: 'Plus Jakarta Sans',
+                                            color: Color(0xFFEE8B60),
+                                            fontSize: 14,
+                                            letterSpacing: 0,
+                                            fontWeight: FontWeight.normal,
+                                          )),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      //le note
+                      Container(
+                        width: 120,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF1F4F8),
+                          borderRadius: BorderRadius.circular(8),
+                          shape: BoxShape.rectangle,
+                          border: Border.all(
+                            color: Color(0xFFE0E3E7),
+                            width: 1,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(item.notes,
+                              style: TextStyle(
+                                fontFamily: 'Outfit',
+                                color: Color(0xFF14181B),
+                                fontSize: 16,
+                                letterSpacing: 0,
+                                fontWeight: FontWeight.w500,
+                              )),
+                        ),
+                      ),
+                      Container(
+                        //da cambiare con la checkBox
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF1F4F8),
+                          borderRadius: BorderRadius.circular(8),
+                          shape: BoxShape.rectangle,
+                          border: Border.all(
+                            color: Color(0xFFE0E3E7),
+                            width: 1,
+                          ),
+                        ),
+                        alignment: AlignmentDirectional(0, 0),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                            size: 32,
+                          ),
+                          onPressed:
+                              onClicked, //richiamerà removeItem nella classe weekPageState
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       );
 }
